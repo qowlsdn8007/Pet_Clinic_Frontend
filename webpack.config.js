20b9c41076fcf8102c80dc79fb2a761a8dc4dee5
qowlsdn8007 <qowlsdn8007@gmail.com>
2021-07-12 17:00:59 +0900

    chore: project init

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const path = require('path');
+
+const prod = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  mode: prod ? 'production' : 'development',
+  devtool: prod ? 'hidden-source-map' : 'eval',
+  entry: './index.tsx',
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
+
+  ///webpack-dev-server
+  devServer: {
+      historyApiFallback: true,
+      inline: true,
+      port: 3000,
+      hot: true,
+      publicPath: '/',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: ['babel-loader', 'ts-loader'],
+      },
+    ],
+  },
+
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js',
+  },
+
+  plugins: [
+	new webpack.ProvidePlugin({
+      React: 'react',
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+    new webpack.HotModuleReplacementPlugin(), // 핫팩 설정
+  ],
+};
+
