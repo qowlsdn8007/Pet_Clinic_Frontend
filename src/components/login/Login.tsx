9edbd8472cb69c6a0c36e3cdaf7133b2c5e80750
LeeSiEun <skvkffish@gmail.com>
2021-07-23 15:36:05 +0900

    chore: move sign up file to components folder

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -38,40 +38,38 @@ function LoginForm() {
 
   return (
     <div className={styles.card}>
-      <div className={styles.login_form}>
-        <Form
-          name="basic"
-          labelCol={{ span: 8 }}
-          wrapperCol={{ span: 0 }}
-          // initialValues={{ remember: true }}
-          onFinish={onFinish}
-          onFinishFailed={onFinishFailed}
-        >
-          <Form.Item name="id" rules={[{ required: true, message: 'id를 입력해주세요!' }]}>
-            <Input placeholder="아이디" />
-          </Form.Item>
-          <Form.Item name="password" rules={[{ required: true, message: 'password를 입력해주세요!' }]}>
-            <Input.Password placeholder="비밀번호" />
-          </Form.Item>{' '}
-          <Form.Item>
-            <Radio.Group onChange={onChange} value={value}>
-              <Radio value={1}>수의사</Radio>
-              <Radio value={2}>동물 병원 직원</Radio>
-              <Radio value={3}>반려동물 보호자</Radio>
-            </Radio.Group>
-          </Form.Item>
-          <Form.Item>
-            <Button type="primary" htmlType="submit">
-              로그인
-            </Button>
-          </Form.Item>
-          <Form.Item name="remember" valuePropName="checked">
-            <Button type="link" onClick={goToSignUp}>
-              회원가입
-            </Button>
-          </Form.Item>
-        </Form>
-      </div>
+      <Form
+        name="basic"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 0 }}
+        // initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+      >
+        <Form.Item name="id" rules={[{ required: true, message: 'id를 입력해주세요!' }]}>
+          <Input placeholder="아이디" />
+        </Form.Item>
+        <Form.Item name="password" rules={[{ required: true, message: 'password를 입력해주세요!' }]}>
+          <Input.Password placeholder="비밀번호" />
+        </Form.Item>{' '}
+        <Form.Item>
+          <Radio.Group onChange={onChange} value={value}>
+            <Radio value={1}>수의사</Radio>
+            <Radio value={2}>동물 병원 직원</Radio>
+            <Radio value={3}>반려동물 보호자</Radio>
+          </Radio.Group>
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            로그인
+          </Button>
+        </Form.Item>
+        <Form.Item name="remember" valuePropName="checked">
+          <Button type="link" onClick={goToSignUp}>
+            회원가입
+          </Button>
+        </Form.Item>
+      </Form>
     </div>
   );
 }
